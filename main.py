345fe924d79285e09b25fe11f5d119bbb8a5efbd
Daniel <danielhakobyan460@gmail.com>
2025-03-14 21:09:32 +0400

    Got back files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -6,6 +8,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 from bson import ObjectId
+
+# Load environment variables from .env file
+load_dotenv()
 
 # FastAPI setup
 app = FastAPI()
@@ -19,12 +24,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-
-# Database connection (using motor for async MongoDB connection)
-MONGO_URI = "mongodb+srv://danielhakobyan460:x8YwO96gnxhsrgXT@cluster0.hl6lw.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = "Final_Project"  # Database name
-COLLECTION_NAME = "issues"  # Collection name
-
+# Get the database connection details from environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")  # Database name
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")  # Collection name
 
 # Set up AsyncIOMotorClient for asynchronous MongoDB operations
 client = AsyncIOMotorClient(MONGO_URI)
@@ -46,23 +49,18 @@ class Issue(BaseModel):
 async def homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Report Issue (Save to MongoDB) - POST route# Report Issue (Save to MongoDB) - POST route
-# POST Route to report issue (Save to MongoDB)# Report Issue (Save to MongoDB) - POST route
+# Report Issue (Save to MongoDB) - POST route
 @app.get("/report-issue", response_class=HTMLResponse)
 async def report_issue_page(request: Request):
     return templates.TemplateResponse("report_issue.html", {"request": request})
 
 @app.post("/report/", response_model=dict)
-
-
 async def report_issue(issue: Issue):
     issue_dict = issue.model_dump()  # Use model_dump() instead of dict()
     result = await issues_collection.insert_one(issue_dict)
     if result.inserted_id:
         return {"message": "Issue reported successfully!", "id": str(result.inserted_id)}
     raise HTTPException(status_code=500, detail="Failed to report issue.")
-
-
 
 # Get All Issues (Retrieve from MongoDB) - GET route
 @app.get("/issues/", response_class=HTMLResponse)
@@ -71,7 +69,6 @@ async def get_issues(request: Request):
     for issue in issues:
         issue["_id"] = str(issue["_id"])  # Convert ObjectId to string
     return templates.TemplateResponse("view_issues.html", {"request": request, "issues": issues})
-
 
 @app.get("/transport")
 async def transport_page(request: Request):
@@ -84,5 +81,3 @@ async def news_page(request: Request):
 @app.get("/city")
 async def city_page(request: Request):
     return templates.TemplateResponse("city.html", {"request": request})
-
-
